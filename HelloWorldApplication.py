132d5219d22bc2d4ddf97c253c539c31ede07ebf
SohyunCho <yourmagenta@gmail.com>
2018-12-15 17:34:45 +0900

    기초자료형

---
+++ HelloWorldApplication.py
@@ -0,0 +1,25 @@
+# a = int(input())
+# b = int(input())
+# print(a % b)
+##########
+# a = input()
+# b = input()
+# print(a + b)
+
+# a="기호"
+# print(a[0])
+
+# a="노엘소현"
+# print(a[2:4])
+
+# a='니가 "밥 먹고 와"라고 말했잖아'
+
+
+# a = input()
+# print(a[:2])
+
+
+a = input()
+b = input()
+print(a)
+print(b[:5])
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 my python first project
+01. 기초자료형공부
+
